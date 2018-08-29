b5a191d9d51417f53d9170016085d9d047a695f3
mkubasz <mkubasz@gmail.com>
2018-08-29 21:46:51 +0200

    Add currency crawler functional

--- InterviewCrawler/App.py
+++ InterviewCrawler/App.py
@@ -1,6 +1,24 @@
+import re
+from typing import Any, List
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-web_content: str = urlopen("https://www.ecb.europa.eu/home/html/rss.en.html")
-html_page = BeautifulSoup(web_content)
-print(html_page)
+
+def read_currency() -> List[str]:
+    postfix_url: str = "https://www.ecb.europa.eu"
+    web_content: str = urlopen("{}/home/html/rss.en.html".format(postfix_url))
+    html_page: Any = BeautifulSoup(web_content, "html.parser")
+    links_url: Any = html_page.select("a[href*=fxref-]")
+    currencies: List[str] = []
+    for link_url in links_url:
+        web_content: str = urlopen("{}{}".format(postfix_url, link_url['href']))
+        html_page = BeautifulSoup(web_content, "html.parser")
+        currencies.append(re.search(r"(\d+\.\d+) ([A-Z]{3})", html_page.text).group())  # Parse currency N.NNN USD
+    return currencies
+
+def save_to_db(currencies: List[str]):
+    return True
+
+
+if __name__ == "__main__":
+    save_to_db(read_currency())
