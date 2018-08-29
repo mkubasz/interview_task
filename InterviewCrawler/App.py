736fc4549994ccbe9f96fbbecd88ef5a3d0ba255
mkubasz <mkubasz@gmail.com>
2018-08-30 00:33:19 +0200

    Implement db connection

--- InterviewCrawler/App.py
+++ InterviewCrawler/App.py
@@ -1,7 +1,11 @@
 import re
+import time
+import os
 from typing import Any, List
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import schedule
+import sqlite3
 
 
 def read_currency() -> List[str]:
@@ -16,9 +20,29 @@ def read_currency() -> List[str]:
         currencies.append(re.search(r"(\d+\.\d+) ([A-Z]{3})", html_page.text).group())  # Parse currency N.NNN USD
     return currencies
 
+
 def save_to_db(currencies: List[str]):
+    pwd = os.path.abspath(os.path.join(os.path.abspath(__file__), '../../InterviewApi/db.sqlite3'))
+    conn = sqlite3.connect(pwd)
+    c = conn.cursor()
+    c.execute('DELETE FROM api_currency')
+    conn.commit()
+    sql = ''' INSERT INTO api_currency(name,value)
+                  VALUES(?,?) '''
+    for currency in currencies:
+        c.execute(sql, (currency.split(" ")[1], currency.split(" ")[0]))
+    conn.commit()
+    conn.close()
     return True
+
+
+def job():
+    save_to_db(read_currency())
 
 
 if __name__ == "__main__":
     save_to_db(read_currency())
+    # schedule.every().day.at("15:16").do(job)
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(60)  # wait one minute
